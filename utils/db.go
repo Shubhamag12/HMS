5b5a6f6566e712f5cadc5e8244f9395656ffb057
Rohitrajak1807 <rohit.rajakah11@gmail.com>
2022-01-18 22:16:17 +0530

    delete unreachable code

--- utils/db.go
+++ utils/db.go
@@ -23,14 +23,12 @@ func CreateGuest(db *sql.DB, guest *models.Guest, payment *models.Payment) (sql.
 	paymentRes, err := CreatePayment(db, payment)
 	if err != nil {
 		log.Panic(err)
-		panic(err)
 	}
 	log.Println(paymentRes)
 	query := "INSERT INTO hotel_man.guest (hotel_id, payment_id, name, check_in_date, check_out_date, room_number) VALUES (?, ?, ?, ?, ?, ?)"
 	res, err := db.Exec(query, guest.HotelId, guest.PaymentId, guest.Name, guest.CheckInDate, guest.CheckOutDate, guest.RoomNumber)
 	if err != nil {
 		log.Panic(err)
-		panic(err)
 	}
 	return res, paymentRes, nil
 }
@@ -40,7 +38,6 @@ func CreatePayment(db *sql.DB, payment *models.Payment) (sql.Result, error) {
 	res, err := db.Exec(query, payment.BookingPayment, payment.IsCheckoutExtended, payment.Overdue)
 	if err != nil {
 		log.Panic(err)
-		panic(err)
 	}
 	return res, nil
 }
