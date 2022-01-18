1550fff3194dc62c07cd12d1b5cf276b34eff137
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2022-01-18 22:15:56 +0530

    Added JSON Config

--- models/hotel.go
+++ models/hotel.go
@@ -1,7 +1,6 @@
 package models
 
 type Hotel struct {
-	Id              int    `json:"id"`
 	Name            string `json:"hotelName"`
 	RoomCount       int    `json:"roomCount"`
 	OoccupiedRooms  int    `json:"roomOcc"`
