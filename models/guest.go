224d4f8c9517276a20a01fddd8034dbde84ba462
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2022-01-18 22:15:39 +0530

    Added JSON config

--- models/guest.go
+++ models/guest.go
@@ -4,6 +4,7 @@ import time
 
 type Guest struct {
 	Id           int    `json:"id"`
+	Payment      int    `json:"payment"`
 	Name         string `json:"name"`
 	CheckInDate  string `json:"inDate"`
 	CheckOutDate string `json:"outDate"`
