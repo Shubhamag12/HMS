08f9d79b3cab47a7561e32d20f7f5dcf5f2ea406
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2022-01-19 15:31:43 +0530

    Updated Routing functions
    add parameters please

--- routes/core.go
+++ routes/core.go
@@ -1,1 +1,20 @@
-package routes
+package route
+
+import (
+	"github.com/Shubhamag12/HMS/utils"
+	"github.com/gorilla/mux"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hotel", utils.GetHotelStats()).Methods("GET")
+	router.HandleFunc("/hotel", utils.ChangeTotalRooms()).Methods("PUT")
+	router.HandleFunc("/guests", utils.GetAllGuests()).Methods("GET")
+	router.HandleFunc("/guests", utils.CreateGuest()).Methods("POST")
+	router.HandleFunc("/guests/{id}", utils.GetGuestsByID()).Methods("GET")
+	router.HandleFunc("/guests/{id}", utils.UpdateCheckOutDate()).Methods("PUT")
+	router.HandleFunc("/guests/{id}", utils.DeleteGuest()).Methods("DELETE")
+
+	return router
+}
