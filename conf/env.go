51a2ee1dde638fd7be4dccfa48fc7eca0ba02658
Rohitrajak1807 <rrajak@platform9.com>
2022-01-20 20:56:11 +0530

    using dovenv, .dev.env has dev configs

--- conf/env.go
+++ conf/env.go
@@ -1,1 +1,17 @@
 package conf
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+var EnvMap map[string]string
+
+func init()  {
+	var err error
+	EnvMap, err = godotenv.Read(".dev.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
