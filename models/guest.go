4f7ef4b514ec2012f7bc43669234030e952d40de
Shubham Agarwal <49315586+Shubhamag12@users.noreply.github.com>
2022-01-22 16:19:32 +0530

    Added Validation Code

--- models/guest.go
+++ models/guest.go
@@ -3,7 +3,7 @@ package models
 type Guest struct {
 	Id           int    `json:"id,omitempty"`
 	Payment      int    `json:"payment,omitempty"`
-	Name         string `json:"name,omitempty"`
-	CheckInDate  string `json:"check_in_date,omitempty"`
-	CheckOutDate string `json:"check_out_date,omitempty"`
+	Name         string `json:"name,omitempty" validate:"required, min=2, max=64"`
+	CheckInDate  string `json:"check_in_date,omitempty" validate:"required, dates"`
+	CheckOutDate string `json:"check_out_date,omitempty" validate:"required, dates"`
 }
