2430fce9d2be8370b3f850bfeb379c5b37ebcfd2
Rohitrajak1807 <rrajak@platform9.com>
2022-01-24 19:19:48 +0530

    add host and port to env

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/Shubhamag12/HMS/conf"
 	"github.com/Shubhamag12/HMS/routes"
 )
 
 func main()  {
 	router := routes.Router()
-	log.Fatal(http.ListenAndServe("127.0.0.1:4000", router))
+	apiBase := fmt.Sprintf("%s:%s", conf.EnvMap["HOST"], conf.EnvMap["PORT"])
+	log.Fatal(http.ListenAndServe(apiBase, router))
 }
